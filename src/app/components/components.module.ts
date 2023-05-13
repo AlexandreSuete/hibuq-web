b3783f5a6ef7f922456f18825dc079da1cd36d02
Alexandre Morales <alexandre.suete@contmatic.com.br>
2023-05-13 17:23:49 -0300

    Primeiro-commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { components } from "./components";
+
+@NgModule(
+    {
+        declarations: [...components],
+        imports: [],
+        exports: [...components],
+    }
+)
+export class ComponentsModule { }
