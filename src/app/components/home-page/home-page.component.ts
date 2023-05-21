635f0c3a018694853474045cbee31204f7cf7c05
Alexandre Morales Suete <alexandre.suete@contmatic.com.br>
2023-05-21 05:03:05 -0300

    Alterações do mobile principal concluidas

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -9,6 +9,7 @@ export class HomePageComponent implements OnInit {
 
   layouts: string[] = ['first-layout', 'second-layout', 'third-layout', 'fourty-layout']; // Add more layouts as needed
   layoutAtual: string = this.layouts[0];
+  public isMobile: boolean = false;
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: any) {
@@ -21,10 +22,16 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.isMobile = this.detectMobile();
   }
 
   public onKeyUp(event: any): void {
     console.log(event);
+  }
+
+  public detectMobile(): boolean {
+    const userAgent = navigator.userAgent;
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
   }
 
 }
